7cf7ee5d188f639dfbaf3465214fc76cdf61c233
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-01-28 17:20:31 -0300

    Update Converter.sh

--- Converter.sh
+++ Converter.sh
@@ -8,3 +8,10 @@ convert /home/guilherme/shell/imagens-livros/algoritmos.jpg  /home/guilherme/she
 
 outro jeito de fazer, passando um parametro
 convert /home/guilherme/shell/imagens-livros/$1.jpg  /home/guilherme/shell/imagens-livros/$1.png
+
+#SEGUNDA VERSÃO MELHORADA:
+
+CAMINHO_IMAGEM=/home/guilherme/shell/imagens-livros
+
+convert $CAMINHO_IMAGEM/$1.jpg $CAMINHO_IMAGEM/$1.png
+convert $CAMINHO_IMAGEM/$2.jpg $CAMINHO_IMAGEM/$2.png
