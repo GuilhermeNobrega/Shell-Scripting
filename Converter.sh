37d24fe2e5459c0328b864998f214f76f62d264e
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-01-28 16:58:42 -0300

    Update Converter.sh

--- Converter.sh
+++ Converter.sh
@@ -5,3 +5,6 @@
 #echo $1
 #echo $0
 convert /home/guilherme/shell/imagens-livros/algoritmos.jpg  /home/guilherme/shell/imagens-livros/algoritmos.png
+
+outro jeito de fazer, passando um parametro
+convert /home/guilherme/shell/imagens-livros/$1.jpg  /home/guilherme/shell/imagens-livros/$1.png
