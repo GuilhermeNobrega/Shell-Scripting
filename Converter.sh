1600b7cf25d66c476bdce5034ae86977d5b64076
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-01-28 16:56:22 -0300

    Update Converter.sh

--- Converter.sh
+++ Converter.sh
@@ -1,7 +1,7 @@
+#PRIMEIRA VERSÃO
+#----------------------------------------------------------------------------------------------------------------
 #!/bin/bash
 
 #echo $1
 #echo $0
-
 convert /home/guilherme/shell/imagens-livros/algoritmos.jpg  /home/guilherme/shell/imagens-livros/algoritmos.png
-
