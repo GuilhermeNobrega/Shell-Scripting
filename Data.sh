69c1484f4f0acea00afd44b15af42a6b03820106
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-03-12 10:21:51 -0300

    Update Data.sh

--- Data.sh
+++ Data.sh
@@ -11,3 +11,23 @@ if [[ "$data" =~ "Tue" ]];
 then
     echo "Today is tuesday"
 fi
+
+#other metod
+
+#!/bin/bash 
+data=$(date)
+#echo "$data"
+for values in $data
+do
+    echo "$values"
+done
+echo "Finished the for"
+sleep 1
+
+finder=$(grep Tue & )
+if [[ "$data" =~ "$finder" ]];
+then
+    echo "Today is tuesday"
+fi
+
+#
