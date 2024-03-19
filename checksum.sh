797ce8ed01e5014d60409131e91fc0e1797eeaa2
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-03-19 00:24:10 -0300

    Update checksum.sh

--- checksum.sh
+++ checksum.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 salvar=("")
 function execution(){
-read -p "Qual arquivo você deseja verificar o código checksum?: " file_name
+while true
+do
+    read -p "Qual arquivo você deseja verificar o código checksum?: " file_name
+    checksum=$(sha256sum "$file_name" | cut -d ' ' -f1)
+    salvar+=("$checksum")
+    #echo "${salvar[@]}"
 
-
-checksum=$(sha256sum "$file_name" | cut -d ' ' -f1)
-salvar+="$checksum"
-echo "${salvar[@]}"
-
-echo "=========================="
-echo "$checksum"
-echo "=========================="
-}
+    echo "=========================="
+    for values in ${salvar[@]}
+    do
+    echo -e "\e$values\e"
+    done
+    echo "=========================="
+    if [ "${salvar[0]}" != "${salvar[1]}" ];
+    then
+        echo "Valores iguais, nada alterado."
+        echo "${salvar[@]}"
+    else
+        echo "Valores diferentes"
+        #echo "${salvar[@]}"
+    fi
+done
+    }
 execution
