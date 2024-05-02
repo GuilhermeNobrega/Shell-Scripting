42627c24bda52e20e9a43483537fefcae7c54764
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-05-02 10:44:16 -0300

    Update Conversor.sh

--- ConversãoNúmerica/Conversor.sh
+++ ConversãoNúmerica/Conversor.sh
@@ -5,7 +5,7 @@ BASE_16_NUM=47295
 
 # Representar o valor em hexadecimal
 # O comando `printf` com o formato `%x` converte `BASE_16_NUM` para hexadecimal.
-printf "%x\n" $BASE_16_NUM
+printf "Hexadecimal -> %x\n" $BASE_16_NUM
 
 # Representar o valor em binário
 # O comando `printf` com o formato `%b` converte `BASE_16_NUM` para binário,
@@ -16,8 +16,8 @@ echo "ibase=10; obase=2; $BASE_16_NUM" | bc
 
 # Representar o valor em octal
 # O comando `printf` com o formato `%o` converte `BASE_16_NUM` para octal.
-printf "%o\n" $BASE_16_NUM
+printf " Octal -> %o\n" $BASE_16_NUM
 
 # Representar o valor em decimal
 # O comando `printf` com o formato `%d` imprime `BASE_16_NUM` em decimal (número inteiro).
-printf "%d\n" $BASE_16_NUM
+printf "Decimal -> %d\n" $BASE_16_NUM
