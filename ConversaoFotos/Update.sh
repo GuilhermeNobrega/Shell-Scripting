798ce7bf3706492a31ce6ea843876d858c25b871
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-05-09 09:08:51 -0300

    Create Update.sh

---
+++ ConversaoFotos/Update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+converte_imagem(){
+    local caminho_imagem=$1
+    local imagem_sem_extensao=$(ls $caminho_imagem | awk -F. '{ print $1 }')
+    convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+}
+
+varrer_diretorio(){
+    cd $1
+    for arquivo in *
+    do
+        local caminho_arquivo=$(find ~/Downloads/imagens-novos-livros -name $arquivo)
+        if [ -d $caminho_arquivo ]
+        then
+            varrer_diretorio $caminho_arquivo
+        else
+            converte_imagem $caminho_arquivo
+        fi
+    done
+}
+
+varrer_diretorio ~/Downloads/imagens-novos-livros
+if [ $? -eq 0 ]
+then
+    echo "Conversão realizada com sucesso"
+else
+    echo "Houve um problema na conversão"
+fi
