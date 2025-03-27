52aac7382ac17a096655005dd7420bfb6cadfe56
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-03-27 14:54:18 -0300

    Update Caesar cipher.sh

--- Caesar cipher.sh
+++ Caesar cipher.sh
@@ -23,3 +23,50 @@ else
 fi
 #d-za-c
 #D-ZA-C
+
+
+#!/bin/bash
+
+array=({a..z})
+arrayuper=({A..Z})
+re='^[0-9]+$'
+read -p "Digite sua frase para descriptografar (CESAR) : " texto
+if [[ $texto =~ $re ]];then
+echo "Erro";
+exit
+fi
+echo "${array[@]}"
+#valores=$(echo "$texto" | tr c-za-bC-ZA-B a-zA-Z)
+#echo "$valores"
+for ((c=1; c<=26; c++))
+do
+    letra="${array[$c]}"
+    letraupper="${arrayuper[$c]}"
+    letra_anterior="${array[$((c-1))]}"
+    letraupper_anterior="${arrayuper[$((c-1))]}"
+    echo "$texto" | tr "$letra-za-$letra_anterior$letraupper-ZA-$letraupper_anterior" "a-zA-Z"
+done
+
+#Other way to do..
+
+array=({a..z})
+arrayuper=({A..Z})
+re='^[0-9]+$'
+read -p "Digite sua frase para descriptografar (CESAR) : " texto
+if [[ $texto =~ $re ]];then
+echo "Erro";
+exit
+fi
+echo "${array[@]}"
+#valores=$(echo "$texto" | tr c-za-bC-ZA-B a-zA-Z)
+#echo "$valores"
+for c in {1..26}
+do
+    letra="${array[$c]}"
+    letraupper="${arrayuper[$c]}"
+    letra_anterior="${array[$((c-1))]}"
+    letraupper_anterior="${arrayuper[$((c-1))]}"
+    echo "$texto" | tr "$letra-za-$letra_anterior$letraupper-ZA-$letraupper_anterior" "a-zA-Z"
+    ((c++));
+done
+
